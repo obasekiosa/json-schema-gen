493ec901aefa5a4fe7a1819bd29b30dec61319e1
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-07-15 07:49:53 +0100

    Scope tests

---
+++ tests.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+class SimpleGeneratorTestCase(unittest.TestCase):
+    pass
+
+# does all schema objects contain tag, required and description fields (must have fields)
+# creating a schema creates the associated file with correct file name
+# schema contains only the message key at the top level
+# schema returns expected output
+# string json comes out with string type
+# integer json comes out with integer type
+# all string array comes out with Enum  type
+# non all string array comes out as array.
+# empty json returns empty schema
+# json with no "message" key returns empty schema
+# invalid json throws error
+# invalid file throws error
+# can must provide only one of file path, json_string, and json_object
+
+
+class SimpleTestSuite(unittest.TestSuite):
+    pass
+
+class GesonTestSuite(unittest.TestSuite):
+    pass 
